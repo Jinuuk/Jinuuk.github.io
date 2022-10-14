ee7f7b62c6b5861b930682d46b91482fc168c2da
Jinuk <zpd29thd03@naver.com>
2022-10-14 17:29:02 +0900

    2022.10.14

---
+++ main.js
@@ -0,0 +1,23 @@
+    // const $greeting = document.getElementById('greeting');
+    // const $introduction = document.getElementById('introduction');
+    // const $skills = document.getElementById('skills');
+    // const $project = document.getElementById('project');
+    // const $education = document.getElementById('education');
+    // const $contact = document.getElementById('contact');
+
+    // const $section_greeting = document.getElementById('section_greeting');
+    // const $section_introduction = document.getElementById('section_introduction');
+    // const $section_skills = document.getElementById('section_skills');
+    // const $section_project = document.getElementById('section_project');
+    // const $section_education = document.getElementById('section_project');
+    // const $section_contact = document.getElementById('section_contact');
+
+    // $greeting.addEventListener('click',e=>{
+    //   e.preventDefault;
+    //   section_greeting.style.display = 'block';
+    //   section_introduction.style.display = 'none';
+    //   section_skills.style.display = 'none';
+    //   section_project.style.display = 'none';
+    //   section_education.style.display = 'none';
+    //   section_contact.style.display = 'none';
+    // });
